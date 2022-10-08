2ded35809f754e3681bba4af6c16c32871bc8e40
Aryman <87179546+rawat-aryman@users.noreply.github.com>
2022-10-09 01:11:09 +0530

    added some other pages and routes to them as well

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-// import logo from './logo.svg';
-// import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <h1>GharDekho.com</h1>
-    </div>
+    <Router>
+      {/* <h1>GharDekho.com</h1> */}
+      <Routes>
+        <Route exact path='/' element={<Explore/>} />
+        <Route path='/ForgotPassword' element={<ForgotPassword/>} />
+        <Route path='/Offers' element={<Offers/>} />
+        <Route path='/Profile' element={<Profile/>} />
+        <Route path='/SignIn' element={<SignIn/>} />
+        <Route path='/SignUp  ' element={<SignUp/>} />
+      </Routes>
+    </Router>
   );
 }
 
