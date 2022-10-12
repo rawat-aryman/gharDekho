5d68a1e7a0865683941e89f4ebafb8f3883b4723
Aryman <87179546+rawat-aryman@users.noreply.github.com>
2022-10-12 22:30:21 +0530

    added slider in listing page using swiperjs

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,8 +3,13 @@ import {Link , useNavigate , useParams} from 'react-router-dom';
 import { getDoc , doc } from 'firebase/firestore';
 import {getAuth} from 'firebase/auth';
 import {db} from '../firebase.config';
+import {MapContainer , Popup , TileLayer , Marker } from 'react-leaflet';
+import SwiperCore , {Navigation , Pagination, Scrollbar, A11y} from 'swiper';
+import {Swiper , SwiperSlide} from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 import Spinner from '../components/Spinner';
 import shareIcon from '../assets/svg/shareIcon.svg';
+SwiperCore.use([Navigation , Pagination, Scrollbar, A11y]);
 
 function Listing() {
 
@@ -38,7 +43,16 @@ function Listing() {
     return (
         <>
             <main>
-                {/* SLIDER */}
+                
+                <Swiper slidesPerView={1} pagination={{clickable:true}}>
+                    {listing.imageUrls.map((url , index) => (
+                        <SwiperSlide key={index}>
+                            <div style={{background : `url(${listing.imageUrls[index]}) center no-repeat`, backgroundSize: 'cover'}} className="swiperSlideDiv">
+
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
 
                 <div className="shareIconDiv" onClick={() =>{
                     navigator.clipboard.writeText(window.location.href);
@@ -79,7 +93,19 @@ function Listing() {
                     </ul>
 
                     <p className="listingLocationTitle">Location</p>
-                    {/* MAP */}
+                        
+                    <div className="leafletContainer">
+                        <MapContainer style={{height: '100%' , width : '100%'}} center={[listing.geoLocation.lat , listing.geoLocation.lng]} zoom={13} scrollWheelZoom={false}>
+                            <TileLayer
+                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+                            />
+
+                            <Marker position={[listing.geoLocation.lat , listing.geoLocation.lng]}>
+                                <Popup>{listing.location}</Popup>
+                            </Marker>
+                        </MapContainer>
+                    </div>
 
                     {auth.currentUser?.uid !== listing.userRef && (
                         <Link to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className='primaryButton'>Contact Landlord</Link>
